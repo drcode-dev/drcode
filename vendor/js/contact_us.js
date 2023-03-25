01126f2f7a94e6b007cc5505efb11ed27431deb9
drcode-dev <32656610+drcode-dev@users.noreply.github.com>
2023-03-25 02:18:00 +0100

    chnage code

--- vendor/js/contact_us.js
+++ vendor/js/contact_us.js
@@ -1,189 +1,42 @@
-//contact us form
-$(".contact_btn").on('click', function () {
-    //disable submit button on click
-    // $(".contact_btn").attr("disabled", "disabled");
-    // $(".contact_btn b").text('Sending');
-    $(".contact_btn i").removeClass('d-none');
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
+require 'vendor/autoload.php';
 
-    //simple validation at client's end
-    var post_data, output;
-    var proceed = "true";
-    // var allBlank;
+$name = $_POST['userName'];
+$email = $_POST['userEmail'];
+$subject = $_POST['userSubject'];
+$message = $_POST['userMessage'];
 
-    var str = $('#contact-form-data').serializeArray();
+$mail = new PHPMailer(true);
 
-    $('#contact-form-data input').each(function() {
-        if(!$(this).val()){
-            // alert('Some fields are empty');
-            proceed = "false";
-        }
-    });
+//Server settings
+$mail->isSMTP();                                            // Send using SMTP
+$mail->SMTPDebug = SMTP::DEBUG_SERVER;
+$mail->Host       = 'smtp.gmail.com';                       // Set the SMTP server to send through
+$mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+$mail->Username   = 'drcode0dev';         // SMTP username
+$mail->Password   = 'oriqteviahjsamec';                   // SMTP password
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;          // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+$mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
-    //everything looks good! proceed...
-    if (proceed === "true") {
+//Recipients
+$mail->setFrom($email, $name);
+$mail->addAddress('drcode0dev@gmail.com');        // Add a recipient
 
-        var pathArray = window.location.pathname.split('/');
-        var secondLevelLocation = pathArray[3];
+// Content
+$mail->isHTML(true);                                        // Set email format to HTML
+$mail->Subject = $subject;
+$mail->Body    = "Name: $name<br>Email: $email<br>Subject: $subject<br>Message: $message<br>";
+$success = $mail->send();
 
-        var accessURL;
-        if(secondLevelLocation){
-            accessURL="./message.php";
-        }else{
-            accessURL="message.php";
-        }
+if ($success) {
+    $result = ['result' => 'success'];
+} else {
+    $result = ['result' => 'error'];
+}
 
-        //data to be sent to server
-        $.post(accessURL, str, function (response) {
-            if (response.type == 'error') {
-                output = '<div class="alert-danger" style="padding:10px 15px; margin-bottom:30px;">' + response.text + '</div>';
-            } else {
-                output = '<div class="alert-success" style="padding:10px 15px; margin-bottom:30px;">' + response.text + '</div>';
-                //reset values in all input fields
-                $('.contact-form input').val('');
-                $('.contact-form textarea').val('');
-            }
-
-            if ($("#result").length) {
-                // alert("yes");
-                $("#result").hide().html(output).slideDown();
-                $(".contact_btn i").addClass('d-none');
-            }else{
-                if (response.type == 'error') {
-                    Swal.fire({
-                        type: 'error',
-                        icon: 'error',
-                        title: 'Oops...',
-                        html: '<div class="text-danger">'+ response.text +'</div>',
-                    })
-                    $(".contact_btn i").addClass('d-none');
-                }else{
-                    Swal.fire({
-                        type: 'success',
-                        icon: 'success',
-                        title: 'Success!',
-                        html: '<div class="text-success">'+ response.text +'</div>',
-                    })
-                    $(".contact_btn i").addClass('d-none');
-                }
-            }
-        }, 'json').fail(function() {
-            alert("Failer");
-        });
-
-    }
-    else
-    {
-        if ($("#result").length) {
-            // alert("yes");
-            output = '<div class="alert-danger" style="padding:10px 15px; margin-bottom:30px;">Please provide the missing fields.</div>';
-            $("#result").hide().html(output).slideDown();
-            $(".contact_btn i").addClass('d-none');
-        }else{
-            Swal.fire({
-                icon: 'error',
-                type: 'error',
-                title: 'Oops...',
-                html: '<div class="text-danger">Please provide the missing fields.</div>'
-            })
-            $(".contact_btn i").addClass('d-none');
-        }
-
-    }
-
-
-});
-
-
-
-
-//modal window form
-
-$(".modal_contact_btn").on('click', function () {
-//disable submit button on click
-// $(".modal_contact_btn").attr("disabled", "disabled");
-// $(".modal_contact_btn b").text('Sending');
-    $(".modal_contact_btn i").removeClass('d-none');
-    //simple validation at client's end
-    var post_data, output;
-    var proceed = "true";
-
-    var str=$('#modal-contact-form-data').serializeArray();
-
-    $('#modal-contact-form-data input').each(function() {
-        if(!$(this).val()){
-            proceed = "false";
-        }
-    });
-
-//everything looks good! proceed...
-    if (proceed === "true") {
-        var pathArray = window.location.pathname.split('/');
-        var secondLevelLocation = pathArray[3];
-
-        var accessURL;
-        if(secondLevelLocation){
-            accessURL="./message.php";
-        }else{
-            accessURL="message.php";
-        }
-
-        //data to be sent to server
-        $.post(accessURL, str, function(response) {
-            alert(response.type);
-
-            if (response.type == 'error') {
-                output = '<div class="alert-danger" style="padding:10px 15px; margin-bottom:30px;">' + response.text + '</div>';
-            } else {
-                output = '<div class="alert-success" style="padding:10px 15px; margin-bottom:30px;">' + response.text + '</div>';
-                //reset values in all input fields
-                $('.contact-form input').val('');
-                $('.contact-form textarea').val('');
-            }
-
-
-            if ($("#quote_result").length) {
-                $("#quote_result").hide().html(output).slideDown();
-                $(".modal_contact_btn i").addClass('d-none');
-            }else{
-                if (response.type == 'error') {
-                    Swal.fire({
-                        type: 'error',
-                        icon: 'error',
-                        title: 'Oops...',
-                        html: '<div class="text-danger">'+ response.text +'</div>',
-                    })
-                    $(".modal_contact_btn i").addClass('d-none');
-                }else{
-                    Swal.fire({
-                        type: 'success',
-                        icon: 'success',
-                        title: 'Success!',
-                        html: '<div class="text-success">'+ response.text +'</div>',
-                    })
-                    $(".modal_contact_btn i").addClass('d-none');
-                }
-            }
-        }).fail(function() {
-            alert("Failer");
-        });
-
-    }
-    else {
-        if ($("#quote_result").length) {
-            output = '<div class="alert-danger" style="padding:10px 15px; margin-bottom:30px;">Please provide the missing fields.</div>';
-            $("#quote_result").hide().html(output).slideDown();
-            $(".modal_contact_btn i").addClass('d-none');
-        }else{
-            Swal.fire({
-                icon: 'error',
-                type: 'error',
-                title: 'Oops...',
-                html: '<div class="text-danger">Please provide the missing fields.</div>'
-            })
-            $(".modal_contact_btn i").addClass('d-none');
-        }
-    }
-});
-
-
-
+header('Content-Type: application/json');
+echo json_encode($result);
+?>
